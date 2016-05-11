e0ee5321bf8e4f1d0359afdb656d60c7b10c8bdc
JasonZYKuang <kzy00007@163.com>
2016-05-11 18:07:24 +0800

    change for sync
    change for sync

--- www/js/services.js
+++ www/js/services.js
@@ -74,6 +74,18 @@ angular.module('starter.services', [])
         };
     })
 
+    .factory('ServerData',function($ionicPopup){
+    	return{
+    		//弹出信息框
+    		alert:function(msg){
+    			$ionicPopup.alert({
+    				template: msg,
+    				title: '提示信息'
+    			});
+    		}
+    	};
+    })
+    
     .factory('DialogueService', function ($http, $q) {
         // Might use a resource here that returns a JSON array
 
